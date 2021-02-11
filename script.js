9500cd6757e129c4c07fd01c78bebc088df44d79
happycooldude <pieterhuisman2000@gmail.com>
2021-02-11 13:09:38 +0100

    De start knop laat de titel en statement  zien en de function die de nieuwe statements moet laten zien werkt ook

---
+++ script.js
@@ -0,0 +1,60 @@
+subjects.forEach(function(subject){ 
+    console.log(subject)
+}
+)
+
+let currentSubject = 0;
+
+const startButton = document.getElementById("startBtn")
+const titleHeader = document.getElementById("Title")
+const statementPar = document.getElementById("Statement")
+const previousbutton = document.getElementById("vorige")
+const nextbutton = document.getElementById("volgende")
+
+startButton.onclick = clickStartBtn;
+previousbutton.onclick = vorige;
+nextbutton.onclick = volgende;
+
+function clickStartBtn() {
+    const container = document.getElementById("container")
+    show(container);
+    show(previousbutton);
+    show(nextbutton);
+
+    titleHeader.innerHTML = subjects[currentSubject].title;
+    statementPar.innerHTML = subjects[currentSubject].statement;
+}
+
+function vorige(){
+    currentSubject --
+    if(currentSubject >=0){
+    titleHeader.innerHTML = subjects[currentSubject].title;
+    statementPar.innerHTML = subjects[currentSubject].statement;
+    }else(currentSubject ++)
+    console.log(currentSubject)
+}
+
+function volgende(){
+    currentSubject ++
+    if(currentSubject<=3){
+    titleHeader.innerHTML = subjects[currentSubject].title;
+    statementPar.innerHTML = subjects[currentSubject].statement;
+    }else(currentSubject --)
+    console.log(currentSubject)
+}
+
+//show element
+function show(element) {
+    element.classList.remove("hidden")
+}
+
+//hide element
+function hide(element) {
+    element.classList.add("hidden")
+}
+
+// DOM Manipulation
+// functions, parameters
+// eventhandlers
+// arrays en objects in javascript
+// changing styles in javascript
