9ce96ae21912d4b5db85848d92f8a716829cbbb0
happycooldude <pieterhuisman2000@gmail.com>
2021-02-15 12:44:16 +0100

    De nieuwe buttens toevoegen die aangeven of je het eens,oneens of geen van beide bent

--- script.js
+++ script.js
@@ -11,10 +11,16 @@ const statementPar = document.getElementById("Statement")
 const previousbutton = document.getElementById("vorige")
 const nextbutton = document.getElementById("volgende")
 const numberOfQuestions = subjects.length
+const eensBTN = document.getElementById("eens")
+const geenVbeideBTN = document.getElementById("geenVbeide")
+const oneensBTN = document.getElementById("oneens")
 
 startButton.onclick = clickStartBtn;
 previousbutton.onclick = vorige;
 nextbutton.onclick = volgende;
+eensBTN.onclick = antwoord;
+geenVbeideBTN.onclick = antwoord;
+oneensBTN.onclick = antwoord;
 
 function clickStartBtn() {
     const container = document.getElementById("container")
@@ -37,11 +43,30 @@ function vorige(){
 
 function volgende(){
     currentSubject ++
-    if(currentSubject<=numberOfQuestions){
+    if(currentSubject<=numberOfQuestions -1){
     titleHeader.innerHTML = subjects[currentSubject].title;
     statementPar.innerHTML = subjects[currentSubject].statement;
     }else(currentSubject --)
     console.log(currentSubject)
+}
+
+function antwoord(){
+    if(this.id=="eens"){
+        volgende;
+        document.getElementById("eens").style.backgroundColor="green"
+        document.getElementById("geenVbeide","oneens").style.backgroundColor="lightgray"
+        console.log(volgende)
+    }
+    else if(this.id=="geenVbeide"){
+        document.getElementById("geenVbeide").style.backgroundColor="green"
+        document.getElementById("eens"&"oneens").style.backgroundColor="lightgray"
+        volgende;
+    }
+    else if(this.id=="oneens"){
+        document.getElementById("oneens").style.backgroundColor="green"
+        document.getElementById("geenVbeide"&"eens").style.backgroundColor="lightgray"
+        volgende;
+    }
 }
 
 //show element
