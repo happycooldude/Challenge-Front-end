7446a3a09a0d47942c76ebe46cdd922453116d29
happycooldude <pieterhuisman2000@gmail.com>
2021-02-18 13:48:33 +0100

    Arrays aangemaakt die gevult worden met de keuze die je gemaakt hebt. nu het onthouden bij de vraag fixen

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-subjects.forEach(function(subject){ 
-    console.log(subject)
-}
-)
+subjects.forEach(function (subject) {
+  console.log(subject);
+});
 
 let currentSubject = 0;
+let answerArray = [];
+let checkArray = [];
 
-const startButton = document.getElementById("startBtn")
-const titleHeader = document.getElementById("title")
-const statementPar = document.getElementById("Statement")
-const previousbutton = document.getElementById("vorige")
-const nextbutton = document.getElementById("volgende")
-const numberOfQuestions = subjects.length
-const eensBTN = document.getElementById("eens")
-const geenVbeideBTN = document.getElementById("geenVbeide")
-const oneensBTN = document.getElementById("oneens")
+const startButton = document.getElementById("startBtn");
+const titleHeader = document.getElementById("title");
+const statementPar = document.getElementById("Statement");
+const previousbutton = document.getElementById("vorige");
+const nextbutton = document.getElementById("volgende");
+const numberOfQuestions = subjects.length;
+const eensBTN = document.getElementById("eens");
+const geenVbeideBTN = document.getElementById("geenVbeide");
+const oneensBTN = document.getElementById("oneens");
 
 startButton.onclick = clickStartBtn;
 previousbutton.onclick = vorige;
@@ -24,78 +25,75 @@ oneensBTN.onclick = antwoord;
 
 //de startknop die alle elementen moet laden
 function clickStartBtn() {
-    const container = document.getElementById("container")
-    show(container);
-    show(previousbutton);
-    show(nextbutton);
-    show(eensBTN);
-    show(geenVbeideBTN);
-    show(oneensBTN);
+  const container = document.getElementById("container");
+  show(container);
+  show(previousbutton);
+  show(nextbutton);
+  show(eensBTN);
+  show(geenVbeideBTN);
+  show(oneensBTN);
 
-    titleHeader.innerHTML = subjects[currentSubject].title;
-    statementPar.innerHTML = subjects[currentSubject].statement;
+  titleHeader.innerHTML = subjects[currentSubject].title;
+  statementPar.innerHTML = subjects[currentSubject].statement;
 }
 
 //scroll door de vragenlijst heen
-function vorige(){
-    currentSubject --
-    if(currentSubject >=0){
+function vorige() {
+  currentSubject--;
+  if (currentSubject >= 0) {
     titleHeader.innerHTML = subjects[currentSubject].title;
     statementPar.innerHTML = subjects[currentSubject].statement;
-    }else(currentSubject ++)
-    console.log(currentSubject)
+  } else currentSubject++;
+  console.log(currentSubject);
 }
 
-function volgende(){
-    currentSubject ++
-    if(currentSubject<=numberOfQuestions -1){
+function volgende() {
+  currentSubject++;
+  if (currentSubject < numberOfQuestions) {
     titleHeader.innerHTML = subjects[currentSubject].title;
     statementPar.innerHTML = subjects[currentSubject].statement;
-    }else(currentSubject --)
-    console.log(currentSubject)
+  } else currentSubject--;
+  console.log(currentSubject);
 }
 
 //geef antwoord op de stelling
-function antwoord(){
-    if(this.id=="eens"){
-        color(eensBTN)
-        uncolor(geenVbeideBTN)
-        uncolor(oneensBTN)
-    }
-    else if(this.id=="geenVbeide"){
-        color(geenVbeideBTN)
-        uncolor(eensBTN)
-        uncolor(oneensBTN)
-    }
-    else if(this.id=="oneens"){
-        color(oneensBTN)
-        uncolor(eensBTN)
-        uncolor(geenVbeideBTN)
-    }
-    volgende();
+function antwoord() {
+  if (this.id == "eens") {
+    color(eensBTN);
+    uncolor(geenVbeideBTN);
+    uncolor(oneensBTN);
+  } else if (this.id == "geenVbeide") {
+    color(geenVbeideBTN);
+    uncolor(eensBTN);
+    uncolor(oneensBTN);
+  } else if (this.id == "oneens") {
+    color(oneensBTN);
+    uncolor(eensBTN);
+    uncolor(geenVbeideBTN);
+  }
+  answerArray.push(this.id);
+  if (answerArray.length >= numberOfQuestions + 1) {
+    answerArray.pop();
+  }
+  console.log(answerArray);
+  volgende();
 }
 
 //geef kleur aan de gekozen keuze
-function color(element){
-    element.style.backgroundColor = "green"
+function color(element) {
+  element.style.backgroundColor = "green";
 }
 
-function uncolor(element){
-    element.style.backgroundColor = "lightgray"
+function uncolor(element) {
+  element.style.backgroundColor = "lightgray";
 }
 
 //show element
 function show(element) {
-    element.classList.remove("hidden")
+  element.classList.remove("hidden");
 }
 
 //hide element
 function hide(element) {
-    element.classList.add("hidden")
+  element.classList.add("hidden");
 }
-
-// DOM Manipulation
-// functions, parameters
-// eventhandlers
-// arrays en objects in javascript
-// changing styles in javascript
